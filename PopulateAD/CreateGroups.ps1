10c30290634bc58aa34f738186c7267acadc1ec3
CMil5 <159480281+CMil5@users.noreply.github.com>
2024-02-20 12:39:04 -0600

    PopulateAD
    A group of scripts used to populate a test AD environment. Primarily used for offline testing of powershell scripts.

---
+++ PopulateAD/CreateGroups.ps1
@@ -0,0 +1,24 @@
+#CMil5 02/20/2024
+
+Import-Module ActiveDirectory
+
+#Import CSV
+$groups = Import-Csv c:\it\groups.csv
+
+
+# Loop through the CSV
+    foreach ($group in $groups) {
+
+    $groupProps = @{
+
+      Name          = $group.name
+      Path          = $group.path
+      GroupScope    = $group.scope
+      GroupCategory = $group.category
+      Description   = $group.description
+
+      }#end groupProps
+
+    New-ADGroup @groupProps
+    
+} #end foreach loop
